2370742e8cd279e0765ad1b01855ce6451931421
Neksha <neksha01@example.com>
2025-10-30 22:44:19 +0530

    exporing http methods

--- src/app.js
+++ src/app.js
@@ -2,13 +2,26 @@ const express = require("express")
 const app = express();
 const port = 3000;
 
+//this will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Neksha",lastname:"Kamboj"})
+})
+
+app.post("/user",(req,res)=>{
+    //saving data to DB
+    res.send("Data successfully send to db")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully");
+    
+})
+// this will match all the http method API calls to /test
 app.use("/test", (req, res) => {
     res.send("Server started ")
 })
 
-app.use("/main", (req, res) => {
-    res.send("another route")
-})
+
 
 app.listen(port, () => {
     console.log("Server started running on port " + port)
