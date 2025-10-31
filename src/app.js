4f2a9edb5a77469c7843266087e2b993c02cd818
Neksha <neksha01@example.com>
2025-10-31 11:14:04 +0530

    updated HTTP methods in app.js

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
 const express = require("express")
 const app = express();
-const port = 3000;
+const port = 7777;
 
-//this will only handle GET call to /user
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Neksha",lastname:"Kamboj"})
-})
-
-app.post("/user",(req,res)=>{
-    //saving data to DB
-    res.send("Data successfully send to db")
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully");
-    
-})
-// this will match all the http method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Server started ")
-})
-
+app.use("/user",(req,res,next)=>{
+    console.log("Route Handler 1");
+     //next();
+    res.send("Response 1");
+   
+},
+(req,res,next)=>{
+    console.log("Route handler 2");
+    next();
+      //res.send("Response 2");
+},
+(req,res,next)=>{
+    console.log("Route handler 3");
+    //res.send("response 3");
+    // next();
+}
+)
 
 
 app.listen(port, () => {
