79e663bf15a387f0f3bac8b2cfa72f7c4c135916
Neksha <neksha01@example.com>
2025-10-31 12:53:23 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -2,41 +2,22 @@ const express = require("express")
 const app = express();
 const port = 7777;
 
-// app.use("/user",(req,res,next)=>{
-//     console.log("Route Handler 1");
-//      //next();
-//     res.send("Response 1");
-   
-// },
-// (req,res,next)=>{
-//     console.log("Route handler 2");
-//     next();
-//       //res.send("Response 2");
-// },
-// (req,res,next)=>{
-//     console.log("Route handler 3");
-//     //res.send("response 3");
-//     // next();
-// }
-// )
-
-const {adminAuth, userAuth}=require("../middlewares/auth")
-// middlewares
-
-app.use("/admin",adminAuth);
-//app.use("/user",userAuth);
-
-
-app.get("/admin/getAllData",(req,res)=>{
-    res.send("Admin data sent");    
+app.get("/getAllData",(req,res)=>{
+    try{
+//logic of DB call
+throw new Error("hscgsuy");
+res.send("Userdata sent")
+    }
+    catch(err){
+        res.status(500).send("Some error contact to support team");
+    }
 })
 
-app.get("/admin/deleteData",(req,res)=>{
-    res.send("delete data");    
-})
 
-app.get("/user",userAuth,(req,res)=>{
-    res.send("User data sent");    
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("some error")
+    }
 })
 
 app.listen(port, () => {
